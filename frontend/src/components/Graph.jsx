002ab01d42b6c0351f2bc8ea1d7978e9a9f7dca4
Jeffrey <sjyjeffrey@qq.com>
2025-09-29 21:45:19 +1000

    add some stylings in graph.jsx

--- frontend/src/components/Graph.jsx
+++ frontend/src/components/Graph.jsx
@@ -1,7 +1,18 @@
 import ForceGraph2D from "react-force-graph-2d";
 import initialGraphData from "../data/GraphData";
+import React, { useState, useEffect, useRef } from "react";
+import { forceCollide } from "https://esm.sh/d3-force-3d";
 
 function Graph(props) {
+  const fgRef = useRef();
+  useEffect(() => {
+    // add collision force
+    fgRef.current.d3Force(
+      "collision",
+      forceCollide((node) => Math.sqrt(100 / (node.val + 1)))
+    );
+  }, []);
+
   let nodeNameIDToGraphID = {};
   initialGraphData.nodes.forEach(
     (node, index) => (nodeNameIDToGraphID[node.id] = index)
@@ -13,7 +24,7 @@ function Graph(props) {
   );
 
   const getNodeLabel = (node) => {
-    return (node.type === "job" ? "Job" : "Skill") + ": " + node.name;
+    return node.name;
   };
 
   const forceGraphData = {
@@ -21,7 +32,7 @@ function Graph(props) {
       return {
         id: node.id,
         name: getNodeLabel(node),
-        val: 1,
+        val: node.value,
       };
     }),
     links: initialGraphData.edges.map((edge, index) => {
@@ -35,9 +46,15 @@ function Graph(props) {
   return (
     <div style={{ width: "100vw", height: "100vh" }}>
       <ForceGraph2D
+        ref={fgRef}
         graphData={forceGraphData}
         nodeLabel="name"
         enableNodeDrag={true}
+        nodeColor={(node) =>
+          nodeGraphIDToOrigNode[nodeNameIDToGraphID[node.id]].type === "skill"
+            ? "red"
+            : "blue"
+        }
         nodeCanvasObjectMode={() => "after"}
         nodeCanvasObject={(node, ctx, globalScale) => {
           const origNode = nodeGraphIDToOrigNode[nodeNameIDToGraphID[node.id]];
@@ -49,6 +66,10 @@ function Graph(props) {
           ctx.fillStyle = "black"; //node.color;
           ctx.fillText(label, node.x, node.y - 6);
         }}
+        nodeRelSize={6}
+        dagMode="zin"
+        backgroundColor="#d0f5bbff"
+        linkColor={() => "rgba(63, 41, 41, 0.2)"}
       />
     </div>
   );
